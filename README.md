777f9d2fa85067f9b50b8857b1e11e48df5cbeab
LorranRocha <youtubebizarro@hotmail.com>
2022-06-19 19:56:16 -0300

    adicionando arquivos

---
+++ README.md
@@ -0,0 +1,14 @@
+# Using Python 3.10.5
+
+# Author: <a href="https://www.lorranrocha.com/">Lorran Rocha</a>
+
+<p>PIP INSTALL</p>
+<ul>
+    <li>PIL</li>
+    <li>termcolor</li>
+    <li>colorama</li>
+</ul>
+
+<br/><br/>
+
+<p>Conversor de imagens para ASCII</p>
---
+++ app.py
@@ -0,0 +1,50 @@
+#   Using Python 3.10.5
+#   Author: Lorran Rocha https://www.lorranrocha.com/
+#   pip intall PIL termcolor colorama
+
+import sys
+from PIL import Image
+from termcolor import colored
+import colorama
+
+# Adicionar imagem
+colorama.init()
+try:
+    image_path = sys.argv[1].strip('-')
+except:
+    image_path = input('Digite o caminho da imagem: ')
+
+# Converter para escala de cinza
+class AsciiArt:
+    def __init__(self, img_path):
+        self.path = image_path
+        self.img = Image.open(self.path)
+
+    def image(self):
+        width, height = self.img.size
+        aspect_ratio = height/width
+        new_width = 120
+        new_height = aspect_ratio * new_width * 0.55
+        img = self.img.resize((new_width, int(new_height)))
+
+        img = img.convert('L')
+        pixels = img.getdata()
+
+        chars = ["B", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."]
+        new_pixels = [chars[pixel//25] for pixel in pixels]
+        new_pixels = ''.join(new_pixels)
+
+        new_pixels_count = len(new_pixels)
+        ascii_image = [new_pixels[index:index + new_width]
+                  for index in range(0, new_pixels_count, new_width)]
+        ascii_image = "\n".join(ascii_image)
+        print(ascii_image)
+
+        # Escrever arquivos de texto
+        file = "ascii_image.txt"
+        with open(file, "w") as f:
+            f.write(ascii_image)
+            print(colored(f"saved art image to file as {file}", "yellow"))
+
+if __name__ == "__main__":
+    AsciiArt(image_path).image()
